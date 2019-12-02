8ba2054cf9e19538997bc9f1698cf6cdf4ef6994
Jrnicus <jrnicus@gmail.com>
2019-12-01 23:53:33 -0500

    Added a response txt file and made it so responder could read it
    So instead of needing all the keys and responses added in the fillResponseMap function I made it so It will read the keys and responses from the file and put them in the map.

--- Responder.java
+++ Responder.java
@@ -14,8 +14,8 @@ import java.util.*;
  * in the HashMap, the corresponding response is returned. If none of the input
  * words is recognized, one of the default responses is randomly chosen.
  * 
- * @author David J. Barnes and Michael Kölling.
- * @version 2016.02.29
+ * @Steve Cate
+ * @11/30/19
  */
 public class Responder
 {
@@ -25,6 +25,7 @@ public class Responder
     private ArrayList<String> defaultResponses;
     // The name of the file containing the default responses.
     private static final String FILE_OF_DEFAULT_RESPONSES = "default.txt";
+    private static final String FILE_OF_SPECIFIC_RESPONSES = "responseHashmap.txt";
     private Random randomGenerator;
 
     /**
@@ -62,57 +63,60 @@ public class Responder
     }
 
     /**
-     * Enter all the known keywords and their associated responses
-     * into our response map.
+     * This will read from a file keys followed by responses on other lines
+     * if there is a blank line that means the next line is a key and the same process continues
+     * until there are two blank lines in a row then we are done reading from the file.
+     * All keys and responses are put into the responseMap to use, if the users types a key in
+     * the map then the response will print out for them.
+     * 
      */
     private void fillResponseMap()
     {
-        responseMap.put("crash", 
-                        "Well, it never crashes on our system. It must have something\n" +
-                        "to do with your system. Tell me more about your configuration.");
-        responseMap.put("crashes", 
-                        "Well, it never crashes on our system. It must have something\n" +
-                        "to do with your system. Tell me more about your configuration.");
-        responseMap.put("slow", 
-                        "I think this has to do with your hardware. Upgrading your processor\n" +
-                        "should solve all performance problems. Have you got a problem with\n" +
-                        "our software?");
-        responseMap.put("performance", 
-                        "Performance was quite adequate in all our tests. Are you running\n" +
-                        "any other processes in the background?");
-        responseMap.put("bug", 
-                        "Well, you know, all software has some bugs. But our software engineers\n" +
-                        "are working very hard to fix them. Can you describe the problem a bit\n" +
-                        "further?");
-        responseMap.put("buggy", 
-                        "Well, you know, all software has some bugs. But our software engineers\n" +
-                        "are working very hard to fix them. Can you describe the problem a bit\n" +
-                        "further?");
-        responseMap.put("windows", 
-                        "This is a known bug to do with the Windows operating system. Please\n" +
-                        "report it to Microsoft. There is nothing we can do about this.");
-        responseMap.put("macintosh", 
-                        "This is a known bug to do with the Mac operating system. Please\n" +
-                        "report it to Apple. There is nothing we can do about this.");
-        responseMap.put("expensive", 
-                        "The cost of our product is quite competitive. Have you looked around\n" +
-                        "and really compared our features?");
-        responseMap.put("installation", 
-                        "The installation is really quite straight forward. We have tons of\n" +
-                        "wizards that do all the work for you. Have you read the installation\n" +
-                        "instructions?");
-        responseMap.put("memory", 
-                        "If you read the system requirements carefully, you will see that the\n" +
-                        "specified memory requirements are 1.5 giga byte. You really should\n" +
-                        "upgrade your memory. Anything else you want to know?");
-        responseMap.put("linux", 
-                        "We take Linux support very seriously. But there are some problems.\n" +
-                        "Most have to do with incompatible glibc versions. Can you be a bit\n" +
-                        "more precise?");
-        responseMap.put("bluej", 
-                        "Ahhh, BlueJ, yes. We tried to buy out those guys long ago, but\n" +
-                        "they simply won't sell... Stubborn people they are. Nothing we can\n" +
-                        "do about it, I'm afraid.");
+        Charset charset = Charset.forName("US-ASCII");
+        Path path = Paths.get(FILE_OF_SPECIFIC_RESPONSES);
+        try (BufferedReader reader = Files.newBufferedReader(path, charset)) {
+            // make the first line a key
+            String key = reader.readLine();
+            // make empty string to use later int he loop
+            String checkResponse = "";
+            String response = "";
+            while(key != null) {
+                // the next line after the key is part of the response
+                checkResponse = reader.readLine();
+                while(checkResponse != null)
+                {
+                    if(checkResponse.equals(""))
+                    {
+                        // add the key and response to the map
+                        responseMap.put(key, response);
+                        //reset value for the next key
+                        response = "";
+                        checkResponse = null;
+                    }
+                    else
+                    {
+                        if(response.equals(""))
+                        {
+                            response += checkResponse;
+                        }
+                        else
+                        {
+                            response += ("\n" + checkResponse);
+                        }
+                        checkResponse = reader.readLine();
+                    }
+                }
+                key = reader.readLine();
+            }
+        }
+        catch(FileNotFoundException e) {
+            System.err.println("Unable to open " + FILE_OF_SPECIFIC_RESPONSES);
+        }
+        catch(IOException e) {
+            System.err.println("A problem was encountered reading " +
+                               FILE_OF_SPECIFIC_RESPONSES);
+        }
+
     }
 
     /**
